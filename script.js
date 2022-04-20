2f122bbfdda395c9c0652e6e053270dc52863fd7
yourfriendra <97575119+yourfriendra@users.noreply.github.com>
2022-04-19 21:42:42 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+// Create integer variable for rock
+const rock = 1
+// Create integer variable for paper
+const paper = 2
+// Create integer variable for scissors
+const scissors = 3
+// Generate random whole number between 1 and 3 and assign it to variable compChoice
+let compChoice = Math.floor(Math.random() * 3 + 1);
+// prompt user to input rock paper or scissors
+alert "Choose rock, paper or scissors!"
+
+//check if misspelled or blank
+if == ""
+// notify and redo if spelled wrong or blank
+
+// if correct use input to create variable userChoice
+
+// make rule to compare answer to the variable computer choice
+
+/* if user answer is rock
+    computer choice of 1 returns tie or
+    computer choice of 2 returns lose or
+    computer choice of 3 returns win
+*\
+//or
+/* if user answer is paper
+    computer choice of 2 returns tie or
+    computer choice of 3 returns lose or
+    computer choice of 1 returns win
+*\
+//or
+/* if user answer is scissors
+    computer choice of 3 returns tie or
+    computer choice of 1 returns lose or
+    computer choice of 2 returns win
+*\
+
+
+// print computer choice
+// print win lose or tie 
+// print message
