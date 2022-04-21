272be5f87def446792c65dfa8923d44a57747d2f
yourfriendra <kw230609@gmail.com>
2022-04-20 21:09:55 -0400

    completed array to determine wins vs losses

--- script.js
+++ script.js
@@ -1,38 +1,30 @@
-//creating array for RPS choices
+function game() {
+
 const RPSOPTIONS = ["rock","paper","scissors"]
-
-function computerPlay () {
-// Generate random whole number between 1 and 3 and use to choose from the option array
-return (RPSOPTIONS [Math.floor(Math.random() * 3 + 1)])
-}
-
-//assign the result from the copmputer play function to computerSelection variable 
-let computerSelection = computerPlay()
-
-console.log( "Computer Chose: " + computerSelection);
-
-// prompt user to input rock paper or scissors and create variable
 let initialChoice = prompt("Choose your fighter", "Rock, Paper or Scissors?");
 
-//convert user input to be case insensitive
+if (initialChoice == null || initialChoice == "") {
+     alert("Please reload and enter a valid choice")
+ }
+
 let userChoice = initialChoice.toLowerCase();
 
-//print to check -_-
-console.log("You Chose: " + userChoice);
-
-//check to see if input is valid choice from the choice array
 if (RPSOPTIONS.indexOf(userChoice) === -1) {
     alert("Enter Valid Choice")
 }
-//check if misspelled or blank
-else if (userChoice == null || userChoice == "") {
-   // notify and redo if spelled wrong or blank
-    alert("RELOAD AND ENTER A CHOICE")
-}
+
+console.log("You Chose: " + userChoice);
+
+function computerPlay () {
+    // Generate random whole number between 1 and 3 and use to choose from the option array
+    return (RPSOPTIONS [Math.floor(Math.random() * 3)])
+    }
+    //assign the result from the copmputer play function to computerSelection variable 
+    let computerSelection = computerPlay()
+    console.log( "Computer Chose: " + computerSelection);
 
 // make function to compare answer to the variable computer choice
 function playRound() {
-
 if (userChoice == computerSelection ) {
     return "Its a Tie";
 }
@@ -44,9 +36,50 @@ else {
 }
 }
 
+console.log((userChoice) + " VS " + (computerSelection) + " : " + (playRound()))
+
+let winStat = playRound();
+return winStat
+
+};
+
+const RECORD = []
+for (let i = 0; i < 5; i++) {
+    RECORD.push(game())
+ }
+
+ function gameStatus () {
+ let winRecord = RECORD.filter(countwins)
+ function countwins (records){
+     return records == "You Win"
+ }
+
+ let lossRecord = RECORD.filter(countloss)
+ function countloss (records){
+     return records == "You Lose"
+ }
+
+ let tieRecord = RECORD.filter(counttie)
+ function counttie (records){
+     return records == "Its a Tie"
+ }
+
+let wins = winRecord.length
+let losses = lossRecord.length
+let ties = tieRecord.length
 
 
+ if (winRecord.length > lossRecord.length) {
+     return (`You won the matches vs the Computer ${wins} to ${losses}! You had ${ties} tied matches.`)
+ }
+ else if (winRecord.length < lossRecord.length) {
+    return (`You lost the matches vs the Computer ${wins} to ${losses}! You had ${ties} tied matches.`)
+ }
 
-// print computer choice
-// print win lose or tie 
-// print message
+ else {
+    return (`You have tied with the Computer! You had ${ties} tied matches and ${wins} and ${losses}.`)
+ }
+
+}
+ console.log(gameStatus())
+ 
